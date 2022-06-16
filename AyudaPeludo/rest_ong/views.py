f785ce27cc23021ee88b2eed28fb793faa93d4d2
mf22222 <m.sepulvedar@profesor.duoc.cl>
2022-06-15 20:12:48 -0400

    Add pet serializer and get pet

--- AyudaPeludo/rest_ong/views.py
+++ AyudaPeludo/rest_ong/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.parsers import JSONParser
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+from core.models import Pet
+from .serializers import PetSerializer
+
+@csrf_exempt
+@api_view(['GET'])
+def lista_pets(request):
+    if request.method == 'GET':
+        pets = Pet.objects.all()
+        pet_serializer = PetSerializer(pets, many=True)
+        return Response(pet_serializer.data)
+
